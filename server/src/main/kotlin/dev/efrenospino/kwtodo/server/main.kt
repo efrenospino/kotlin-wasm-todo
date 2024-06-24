a090b9f01466581dc2c55ddefdf768e98dce7271
Efren Ospino <efrenospinojr@gmail.com>
2024-06-24 10:29:42 -0500

    chore: use PORT from environment variables on server module

--- server/src/main/kotlin/dev/efrenospino/kwtodo/server/main.kt
+++ server/src/main/kotlin/dev/efrenospino/kwtodo/server/main.kt
@@ -11,15 +11,16 @@ import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
+val env: MutableMap<String, String> = System.getenv()
+
 fun main() {
     embeddedServer(
         Netty,
-        port = 8080,
+        port = env["PORT"]?.toInt() ?: 8080,
         host = "0.0.0.0",
         module = Application::module
     ).start(wait = true)
 }
-
 
 fun Application.module() {
 
