f2f56468a63392d39ef1b08d1f1bf8aaa4998981
Efren Ospino <efrenospinojr@gmail.com>
2024-06-24 08:46:29 -0500

    chore: delete not relevant file

--- shared/src/commonMain/kotlin/dev/efrenospino/kwtodo/Constants.kt
+++
@@ -1,7 +0,0 @@
-package dev.efrenospino.kwtodo
-
-object Constants {
-    val SERVER_SCHEMA: String = BuildKonfig.SERVER_SCHEMA
-    val SERVER_HOST: String = BuildKonfig.SERVER_HOST
-    val SERVER_PORT: Int = BuildKonfig.SERVER_PORT
-}
